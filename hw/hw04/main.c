e36bb4190ff6263893d8774802ed5ab77731c852
OlliJeCool <ollihomail@gmail.com>
2024-11-01 08:30:20 +0100

    hw04 progress

--- hw/hw04/main.c
+++ hw/hw04/main.c
@@ -3,43 +3,48 @@
 
 int takeAndValidateInput(long int *, int *);
 void printError(int);
-int* generatePrimeArray(int *);
-int* calculateInPrimes(int, int*, int);
-void printOutput(int*, int*, int);
+void evaluateNums(_Bool*, int *);
+void calculateInPrimes(int, _Bool*, int*, int);
+void printOutput(_Bool*, int*, int);
+void extractPrimes(int*, _Bool*);
+
 
 int main() {
     long int input = 0;
-    int ret = 0;
-    int primes_count = 0;
-    int* primes = generatePrimeArray(&primes_count);
+    int ret = 0, primes_count = 0;
+    _Bool primes[MAX+1];
+    evaluateNums(primes, &primes_count);
+    int primeOnly[primes_count];
+    extractPrimes(primeOnly, primes);
     while(takeAndValidateInput(&input, &ret) == 1) {
+        printf("count: %d", primes_count);
+        printf("ret: %d", ret);
         if(!ret) {
-            int* powers = calculateInPrimes(input, primes, primes_count);
+            int powers[primes_count];
+            calculateInPrimes(input, primes, powers, primes_count);
             printOutput(primes, powers, primes_count);
         }
     }
     return 0;
 }
 
-void printOutput(int* primes, int* powers, int count) {
+void printOutput(_Bool* primes, int* powers, int count) {
     for(int i = 0; i < count; i++) {
         if(powers[i] != 0) {
-            printf(powers[i] == 1 ? "%d x" : "%d^%d"  , primes[i], powers[i]);
+            printf(powers[i] == 1 ? "%d" : "x %d^%d"  , primes[i], powers[i]);
         }
     }
 }
 
-int* calculateInPrimes(int num, int* primes, int count) {
+void calculateInPrimes(int num, _Bool* primes, int* powers, int count) {
     int temp = num;
-    int nums[count];
     for(int i = count; i >= 0; i--) {
         if(temp % primes[i] == 0) {
             temp = num / primes[i];
-            nums[i]++;
+            powers[i]++;
             i++;
         }
     }
-    return nums;
 }
 
 void printError(int ret) {
@@ -49,45 +54,36 @@ void printError(int ret) {
 }
 
 int takeAndValidateInput(long int* input, int* ret) {
-    int result = scanf("%d", input);
+    int result = scanf("%ld", input);
     if(input < 0 || result == 0) {
         *ret = 100;
     }
     return result;
 }
 
-void extractPrimes(int* primes_count, int* primesOnly, int* primes) {
+void extractPrimes(int* primeOnly, _Bool* primes) {
     int index = 0;
-    for(int i = 0; i < MAX; i++) {
-        if(primes[i]) {
-
+    for(int h = 0; h < MAX; h++) {
+        if(*(primes+h)) {
+            index++;
+            *(primeOnly+index) = h;
         }
     }
 }
 
 
-int* evaluateNums(int* primes_count) {
-    _Bool arr[MAX+1];
+void evaluateNums(_Bool *primes, int* primes_count) {
     for(int i = 2; i <= MAX; i ++) {
-        arr[i] = 1;
+        *(primes+i) = 1;
     }
     for(int j = 2; j * j <= MAX; j++) {
-        if(arr[j]) {
+        if(*(primes+j)) {
+            *primes_count += 1;
             for(int k = j * j; k <= MAX; k += j) {
-                if(arr[k] != 0) {
-                    arr[k] = 0;
+                if(*(primes+k) != 0) {
+                    *(primes+k) = 0;
                 }
             }
         }
     }
-
-    static int primeOnly[sizeof(arr)-2-*primes_count];
-    int index = 0;
-    for(int h = 0; h < MAX; h++) {
-        if(arr[h]) {
-            primeOnly[index++] = h;
-        }
-    }
-    *primes_count = sizeof(arr)-2-*primes_count;
-    return primeOnly;
 }
